a9a4c1d9f2049fb926222578bc24e6a2d7527893
deenzi <dinah.bronner@gmail.com>
2022-02-13 20:38:03 +0100

    deployed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           <p>
-         Hello! <Weather/>
+         Hello! lets deploy <Weather/>
         </p>
         </div>
 
